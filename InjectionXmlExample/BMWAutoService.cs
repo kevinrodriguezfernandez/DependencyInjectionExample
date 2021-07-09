33ce75643d62f1985f5fcba11cdea67ebe9b8295
Kevin Rodriguez <kevinrodriguez@capitole-consulting.com>
2021-07-09 18:01:55 +0200

    Injection with xml File

---
+++ InjectionXmlExample/BMWAutoService.cs
@@ -0,0 +1,13 @@
+﻿using InjectionXmlExample.Contracts;
+using System;
+
+namespace InjectionXmlExample
+{
+    public class BMWAutoService : AutoService
+    {
+        public void GetService()
+        {
+            Console.WriteLine("You chose BMW auto service");
+        }
+    }
+}
---
+++ InjectionXmlExample/HondaAutoService.cs
@@ -0,0 +1,13 @@
+﻿using InjectionXmlExample.Contracts;
+using System;
+
+namespace InjectionXmlExample
+{
+    public class HondaAutoService:AutoService
+    {
+        public void GetService()
+        {
+            Console.WriteLine("You chose Honda auto service");
+        }
+    }
+}
